461f7419afe6dd78d4d6601c6fd897037eb0f1d5
Jivayu <9782164+Jivayu@users.noreply.github.com>
2024-11-11 18:43:07 +0530

    Commit from TeleportHQ

--- components/contact-info.js
+++ components/contact-info.js
@@ -39,7 +39,7 @@ const ContactInfo = (props) => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  className="contact-info-icon10 thq-icon-medium"
+                  className="contact-info-icon10"
                 >
                   <g fill="none">
                     <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path>
@@ -79,7 +79,7 @@ const ContactInfo = (props) => {
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
-                  className="contact-info-icon14 thq-icon-medium"
+                  className="contact-info-icon14"
                 >
                   <g
                     fill="none"
@@ -477,8 +477,8 @@ const ContactInfo = (props) => {
             border-color: var(--dl-color-primary-logo-color-5);
             border-style: solid;
             border-width: 1px;
-            padding-left: var(--dl-space-space-halfunit);
-            padding-right: var(--dl-space-space-halfunit);
+            padding-left: 0px;
+            padding-right: 0px;
             flex-direction: column;
             padding-bottom: var(--dl-space-space-unit);
             background-size: auto;
@@ -615,7 +615,6 @@ const ContactInfo = (props) => {
           }
           .contact-info-button {
             flex: 1;
-            display: none;
             transform: scale(0.96);
             align-self: center;
             transition: 0.3s;
